98be02e7d65170e01976631d2301162b78689ec2
UH.Kim <kimhr0703@gmail.com>
2016-09-20 16:18:25 +0900

    add simple-enabling fct.

--- node-link.cc
+++ node-link.cc
@@ -17,16 +17,46 @@ namespace LinkNode{
     using v8::String;
     using v8::Value;
 
+    static InterfaceTable *ft;
+
+    static ableton::Link *gLink = NULL;
+    static float gTempo = 60.0;
+
     void Method(const FunctionCallbackInfo<Value>& args) {
       Isolate* isolate = args.GetIsolate();
       args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
     }
 
 
+    void enableLink(const FunctionCallbackInfo<Value>& args) {
+      Isolate* isolate = args.GetIsolate();
+
+      ableton::Link link(gTempo);
+      link.enable(true);
+      gLink = &link;
+
+      args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+    }
+    void disableLink(const FunctionCallbackInfo<Value>& args) {
+      Isolate* isolate = args.GetIsolate();
+      if (gLink!=NULL){
+          gLink.enable(false);
+      }
+      args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+    }
+
+
+
+
     //node
     void init(Local<Object> exports) {
     //Method를 Hello라는 이름으로 매칭 시킨다->addon.hello()
       NODE_SET_METHOD(exports, "hello", Method);
+
+      NODE_SET_METHOD(exports, "enable", enableLink);
+      NODE_SET_METHOD(exports, "disable", disableLink);
+
+
     }
 
     NODE_MODULE(LinkNode, init)
